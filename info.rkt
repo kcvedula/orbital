f8c7b422f72cce4f2a1a2b8d383d51b5a70965c4
IRONM00N <me@ironmoon.dev>
2025-03-30 18:00:36 -0400

    start making into pkg

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "orbital")
+(define deps '("base" "pict3d"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/orbital.scrbl" ())))
+(define pkg-desc "3D Molecular Rendering")
+(define version "0.1.0")
+(define pkg-authors '(kcvedula ironmoon))
+(define license '(GPL-3.0-or-later))
---
+++ scribblings/orbital.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[orbital
+                    racket/base]]
+
+@title{orbital}
+@author{ironmoon}
+
+@defmodule[orbital]
+
+Package Description Here
