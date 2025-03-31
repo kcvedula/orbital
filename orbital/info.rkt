322df52593063da5d9f580a75af6cb2cb73dee5b
IRONM00N <me@ironmoon.dev>
2025-03-30 20:05:35 -0400

    fix info.rkt

--- orbital/info.rkt
+++ orbital/info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "orbital")
-(define deps '("base" "pict3d" "typed-racket-lib"))
+(define deps '("base" "pict3d" "typed-racket-lib" "typed-racket-more"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/orbital.scrbl" ())))
 (define pkg-desc "3D Molecular Rendering")
-(define version "0.1.0")
+(define version "0.1")
 (define pkg-authors '(kcvedula ironmoon))
 (define license '(GPL-3.0-or-later))
