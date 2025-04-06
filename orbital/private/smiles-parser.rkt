9e48b59a1b42aad459820cfa1d1077a0395617f6
IRONM00N <me@ironmoon.dev>
2025-04-06 03:11:22 -0400

    (it was not infact fixed)

--- orbital/private/smiles-parser.rkt
+++ orbital/private/smiles-parser.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (require parser-tools/lex
-         parser-tools/yacc)
+         parser-tools/yacc
+         (prefix-in : parser-tools/lex-sre))
 
 ;; <smiles>            ::= <chain>
 ;; <chain>             ::= <bond> <branched-atom>
@@ -51,7 +52,16 @@
 ;; <digit>             ::= '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 ;; <number>            ::= <digit> <digit>*
 
-(define-tokens value-tokens (NUMBER DIGIT))
+(define-tokens value-tokens (NUMBER DIGIT SYMBOL))
+
+(define inside-brackets? (make-parameter #f))
+
+
+(define elements
+  '(H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
+      Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm
+      Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn Fr Ra Ac Th Pa U Np Pu Am Cm Bk Cf Es Fm Md No
+      Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og))
 
 (define-empty-tokens tokens
   (LBRACKET RBRACKET LPAREN RPAREN
@@ -65,10 +75,10 @@
             EOF))
 
 (define smiles-lexer
-  (lexer-src-pos
+  (lexer
    [(eof) (token-EOF)]
-   [#\[ (token-LBRACKET)]
-   [#\] (token-RBRACKET)]
+   [#\[ (begin (inside-brackets? #t) (token-LBRACKET))]
+   [#\] (begin (inside-brackets? #f) (token-RBRACKET))]
    [#\( (token-LPAREN)]
    [#\) (token-RPAREN)]
    [#\. (token-DOT)]
@@ -76,7 +86,6 @@
    [#\+ (token-PLUS)]
    [#\- (token-MINUS)]
    [#\: (token-COLON)]
-   [#\% (token-PERCENT)]
    ["@@" (token-AT_AT)]
    [#\@ (token-AT)]
    [#\H (token-H_COUNT)]
@@ -99,25 +108,49 @@
    ["/" (token-BOND_UP)]
    ["\\" (token-BOND_DOWN)]
    ["-" (token-BOND_SINGLE)]
-   [(repetition 2 +inf.0 numeric) (token-NUMBER (string->number lexeme))]
-   [numeric (token-DIGIT (string->number lexeme))]))
+   [(:: upper-case (:? lower-case))
+    (if (member (string->symbol lexeme) elements)
+        (token-SYMBOL lexeme)
+        (begin
+          (printf "Skipping invalid element: ~a\n" lexeme)
+          (smiles-lexer input-port)))]
+   
+   ;; Multi-digit numbers in brackets only (e.g., 13, 18, 100)
+   [(repetition 2 +inf.0 numeric)
+    (if (inside-brackets?)
+        (token-NUMBER (string->number lexeme))
+        (let* ([first (substring lexeme 0 1)]
+               [rest  (substring lexeme 1)]
+               [rest+port (open-input-string (string-append rest (port->string input-port)))])
+          (set! input-port rest+port)
+          (token-DIGIT (string->number first))))]
+
+   ;; Single-digit numbers always valid
+   [numeric (token-DIGIT (string->number lexeme))]
+
+   ;; Percent-prefixed ring numbers: always NUMBER after %
+   [(:seq "%" (:* numeric))
+    (let* ([digits (substring lexeme 1)]
+           [num (string->number digits)])
+      (values
+       (token-PERCENT)
+       (token-NUMBER num)))]))
 
 (define smiles-parser
   (parser
-   (src-pos)
    (start smiles)
    (end EOF)
    (tokens value-tokens tokens)
-   (error (lambda (tok? name val start end)
-            (error "Parse error on token" tok? name val start end)))
+   (error (lambda (tok? name val)
+            (error "Parse error on token" tok? name val)))
 
    (grammar
     [smiles ((chain) $1)]
 
     [chain ((bond branched-atom) (list (list 'bond $1) $2)) 
+           ((branched-atom) (list $1))
            ((chain ring-bond) (append $1 (list $2)))
            ((chain bond ring-bond) (append $1 (list (list 'bond $2) $3))) 
-           ((branched-atom) (list $1))
            ((chain branched-atom) (append $1 (list $2)))
            ((chain bond branched-atom) (append $1 (list (list 'bond $2) $3)))
            ((chain dot branched-atom) (append $1 (list (list 'bond '\.) $3)))]
@@ -154,7 +187,9 @@
     [isotope? ((NUMBER) `(isotope ,$1))
               ((DIGIT) `(isotope ,$1))
               (() #f)]
-    [symbol ((aliphatic-organic) `(symbol ,$1))
+    
+    [symbol ((SYMBOL) `(symbol ,$1))
+            ((aliphatic-organic) `(symbol ,$1))
             ((aromatic-organic)  `(symbol ,$1))
             ((STAR) '(symbol *))]
 
@@ -192,7 +227,25 @@
   (for ([s '("C" "CC" "C(C)C" "[13CH3]" "[O-]" "C=C" "F/C=C/F" 
                  "C1CCC1" "CC(=O)CC" "CCCCCCCC" "CCCC1CC1"
                  "COc(c1)cccc1C#N"
-                 "N1CCN(CC1)C(C(F)=C2)=CC(=C2C4=O)N(C3CC3)C=C4C(=O)O")])
+                 "N1CCN(CC1)C(C(F)=C2)=CC(=C2C4=O)N(C3CC3)C=C4C(=O)O"
+                 
+                 "N#N" ; Dinitrogen
+                 "CN=C=O" ; Methyl isocyanate
+                 "[Cu+2].[O-]S(=O)(=O)[O-]" ; Copper(II) sulfate
+                 "O=Cc1ccc(O)c(OC)c1" ; Vanillin
+                 "COc1cc(C=O)ccc1O"
+                 "CC(=O)NCCC1=CNc2c1cc(OC)cc2" ; Melatonin
+                 "CC(=O)NCCc1c[nH]c2ccc(OC)cc12"
+                 "CCc(c1)ccc2[n+]1ccc3c2[nH]c4c3cccc4" ; Flavopereirin 
+                 "CCc1c[n+]2ccc3c4ccccc4[nH]c3c2cc1"
+                 "CN1CCC[C@H]1c2cccnc2" ; Nicotine
+                 "CCC[C@@H](O)CC\\C=C\\C=C\\C#CC#C\\C=C\\CO" ; Oenanthotoxin 
+                 "CCC[C@@H](O)CC/C=C/C=C/C#CC#C/C=C/CO"
+                 "CC1=C(C(=O)C[C@@H]1OC(=O)[C@@H]2[C@H](C2(C)C)/C=C(\\C)/C(=O)OC)C/C=C\\C=C" ; Pyrethrin II
+                 "O1C=C[C@H]([C@H]1O2)c3c2cc(OC)c4c3OC(=O)C5=C4CCC(=O)5" ; Aflatoxin B_1
+
+
+                 )])
     (printf "~a\n=>\n" s)
     (pretty-print (parse-smiles s))
     (newline)))
