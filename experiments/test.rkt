07d3e45440c5a5ba488c800b5cb53d60158407fb
IRONM00N <me@ironmoon.dev>
2025-04-21 20:35:02 -0400

    rm experiments, old

--- experiments/test.rkt
+++
@@ -1,9 +0,0 @@
-#lang typed/racket
-(require pict3d)
-(require "explore.rkt")
-(require "functional-groups.rkt")
-(require "orbital1.rkt")
-
-#; (explore (sp3 OXYGEN #:d1 lone-pair #:d2 lone-pair #:d3 -H #:R -H))
-(explore (aryl #:d1 -Iodine #:d2 -Cl #:d3 -Br #:d4 (carbon-chain 8) #:R -H))
-
